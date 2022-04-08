42d44f07d7675d676a6c8daedd992c7bc4aebbe9
Nikolay <nyk190@mail.ru>
2022-04-09 01:15:22 +0300

    Create index.html, style.css, script.js
    Create index.html, style.css, script.js. Add html boilerplate, also
    create first version of main math functions of calculator.

---
+++ script.js
@@ -0,0 +1,23 @@
+function addition(numberOne, numberTwo) {
+    return numberOne + numberTwo;
+}
+
+function subtract(numberOne, numberTwo) {
+	return numberOne - numberTwo;
+}
+
+function multiply(numberOne, numberTwo) {
+	return numberOne * numberTwo;
+}
+
+function division(numberOne, numberTwo) {
+	return numberOne / numberTwo;
+}
+
+function percent(number) {
+	return number/100;
+}
+
+function signChange(number) {
+    return (number <= 0) ? +number: -number;
+}
