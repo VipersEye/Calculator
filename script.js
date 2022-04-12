dbc0d225e3c3854eb8e07ae93a01b86fbd64819e
Nikolay <nyk190@mail.ru>
2022-04-12 19:49:44 +0300

    Add operator function
    Add operator function, which get operator and two numbers and depend
    on operator return result of calculation.

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-function addition(numberOne, numberTwo) {
-    return numberOne + numberTwo;
-}
-
-function subtract(numberOne, numberTwo) {
-	return numberOne - numberTwo;
-}
-
-function multiply(numberOne, numberTwo) {
-	return numberOne * numberTwo;
-}
-
-function division(numberOne, numberTwo) {
-	return numberOne / numberTwo;
-}
-
-function percent(number) {
-	return number/100;
-}
-
-function signChange(number) {
-    return (number <= 0) ? +number: -number;
+function operator(operator, numOne, numTwo = NaN) {
+	let res = NaN;
+	switch(operator) {
+		case '+':
+			res = numOne + numTwo;
+			break;
+		case '-':
+			res = numOne - numTwo;
+			break;
+		case '*':
+			res = numOne * numTwo;
+			break;
+		case '/':
+			res = numOne / numTwo;
+			break;
+		case '%':
+			res = numOne / 100;
+			break;
+		case '+-':
+			res = -numOne;
+			break;
+		case '1/x':
+			res = 1 / numOne;
+			break;
+		case '^2':
+			res = numOne**2;
+			break;
+		case 'sqrt':
+			res = Math.sqrt(numOne);
+			break;
+	}
+	return isNaN(res) || !isFinite(res) ? alert('Something went wrong... Try again') : res;
 }
