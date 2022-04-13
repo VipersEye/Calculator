47bd9a771f1f331833495eb3be17905e26a0af7a
Nikolay <nyk190@mail.ru>
2022-04-13 23:23:56 +0300

    Add numLimitCheck and deleteLastNumeral functions
    Add numLimitCheck which check length of number in display. If it more
    than 16, then calls deleteLastNumeral function, which delete last
    numeral of number.

--- script.js
+++ script.js
@@ -41,12 +41,32 @@ const calculator = {
 let display = document.querySelector('.display_number');
 let btnsNumber = document.querySelectorAll('.btn_number');
 
+let btnDeleteLast = document.querySelector('.btn_function[value=Del]');
+
 btnsNumber.forEach(btn => {
 	btn.addEventListener('click', evt=>{
 		displayAddNumber(evt.target.value);
+        numLimitCheck();
 	});
 });
 
+btnDeleteLast.addEventListener('click', deleteLastNumeral);
+
+
+
+
+
+
 function displayAddNumber(number) {
     display.value = (display.value).toString() + number.toString();
+}
+
+function numLimitCheck() {
+    if (display.value.toString().length > 16) {
+        deleteLastNumeral();
+    }
+}
+
+function deleteLastNumeral() {
+    display.value = display.value.slice(0, display.value.toString().length - 1);
 }
